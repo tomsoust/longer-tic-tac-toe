914925d7aa7e551e95d65aa17862e669517aa063
Tom Soust <tomsoust@gmail.com>
2023-09-07 12:44:32 +1000

    updating style

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -28,6 +28,16 @@ const startGame = () => {
 }
 restartBtn.addEventListener('click', handleRestart)
 
+const winningCombo = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 // utility functions
 let moves = 0
@@ -60,16 +70,6 @@ function updateScoreboard() {
 }
   
 
-const winningCombo = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
 
 
 function playerWins() {
